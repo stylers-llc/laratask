e9595329a829e2a9bec7c07a1d1c156be3e4e091
Szilveszter Nagy <t1k3hu@gmail.com>
2018-03-05 14:51:36 +0100

    Add TaskTemplate FormRequests

---
+++ src/Requests/UpdateTaskTemplate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Stylers\Laratask\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Stylers\Laratask\Interfaces\AssignableInterface;
+use Stylers\Laratask\Interfaces\TaskableInterface;
+use Stylers\Laratask\Rules\ImplementsInterface;
+use Stylers\Taxonomy\Models\Language;
+
+class UpdateTaskTemplate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $code = $this->getDefaultLanguageCode();
+
+        return [
+            'name' => 'required|array',
+            'name.translations.' . $code => 'required|string',
+
+            'assignable_type' => ['required', 'string', new ImplementsInterface(AssignableInterface::class)],
+            'assignable_id' => 'required|numeric',
+
+            'taskable_type' => ['string', new ImplementsInterface(TaskableInterface::class)],
+            'taskable_id' => 'numeric',
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    private function getDefaultLanguageCode()
+    {
+        return Language::getDefaultLanguageCode();
+    }
+}
