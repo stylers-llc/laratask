144e549845a8e4ece6cdef997497c4824358cad7
Szilveszter Nagy <t1k3hu@gmail.com>
2018-03-23 09:16:56 +0100

    Add TaskTemplateRuntime (store, update) requests

---
+++ src/Requests/UpdateTaskTemplateRuntime.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Stylers\Laratask\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Stylers\Laratask\Rules\IsDateInterval;
+
+class UpdateTaskTemplateRuntime extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'start_at' => 'required|date',
+            'end_at' => 'date|after:start_at',
+            'date_interval' => [new IsDateInterval()],
+        ];
+    }
+}
